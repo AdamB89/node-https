8d0cbe1a97b30b78d64fd9a38fc6de84f9be4e63
Adam Baram <adam_baram@windowslive.com>
2019-07-30 22:39:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var https = require('https')
+var fs = require('fs');
+var path = require('path')
+
+var options = {
+    key: fs.readFileSync(path.join(__dirname,'bin','private.key')),
+    cert: fs.readFileSync(  path.join(__dirname,'bin','certificate.pem'))
+};
+
+
+var app= express();
+
+app.set('secPort',443);
+
+app.use('/', (req,res)=>{
+        res.send('<h1>Hello Nemesh</h1>')
+})
+var secureServer = https.createServer(options,app);
+
+app.listen(3000,()=>{
+    console.log('app running at port 3000')
+})
+
+secureServer.listen(app.get('secPort'), () => {
+    console.log('Server listening on port ',app.get('secPort'));
+ });
+ secureServer.on('error', ()=>{
+     console.log("error")
+ });
+ secureServer.on('listening',()=>{
+     console.log("start listening to secure port")
+ });
